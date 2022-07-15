1f5859891405624725b744179abcd7dfbd9aef96
Benjamin 1er <bgeron@coton.dev>
2022-07-15 16:04:15 +0200

    React Router Dom

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,1 +1,11 @@
-export const Router = () => <div />;
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export const Router = (): JSX.Element => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element='' />
+      </Routes>
+    </BrowserRouter>
+  )
+}
